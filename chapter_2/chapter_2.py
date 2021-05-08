0a7205aa5324fb8848ab3c64cfd493763edec574
aarondoerfler <aaron.doerfler17@gmail.com>
2021-05-08 16:53:59 -0400

    used an if else statement to print my name

---
+++ chapter_2/chapter_2.py
@@ -0,0 +1,9 @@
+name = 'Aaron'
+password = 'swordfish'
+
+if name == 'Aaron':
+    print('Hello, Aaron')
+    if password == 'swordfish':
+        print('Access Granted.')
+    else:
+        print('Wrong Password.')
