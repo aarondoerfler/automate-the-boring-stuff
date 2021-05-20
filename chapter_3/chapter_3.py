73de8ede9e4f040fd29af98204ab0d03119954b1
aarondoerfler <aaron.doerfler17@gmail.com>
2021-05-19 20:55:10 -0400

    added chapter 3 python file and completed first few exercises

---
+++ chapter_3/chapter_3.py
@@ -0,0 +1,13 @@
+def hello():
+    print('Howdy')
+    print('Howdy!!!')
+    print('Hello There')
+
+hello()
+hello()
+hello()
+
+def hello(name):
+    print('Hello,' + name)
+hello('Alice')
+hello('Bob')
